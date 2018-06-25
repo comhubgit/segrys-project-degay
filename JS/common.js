f12024858b3a4d48c72a6d2d84dfb43a756578c4
comhubgit <39833686+comhubgit@users.noreply.github.com>
2018-06-25 19:27:53 +0300

    Update common.js

--- JS/common.js
+++ JS/common.js
@@ -1,23 +1,25 @@
+
 let budgetInput = document.getElementById('budget-input');
 let button = document.getElementById('budget-btn');
 let dayInput = document.getElementById('day-input');
 let i;
 button.addEventListener('click', function() {
-  let budget = budgetInput.value;
-  let dayz = dayInput.value;
-  if (isNaN(budget)) {
-    alert('В поле бюджета введено не число!')
-  }
-  if (isNaN(dayz)) {
-    alert('В поле дней введено не число!')
-  }
-  dayzBudget = budget / dayz;
-  for (i = 1; i < dayz + 1; i++) {
-    console.log(Math.floor(dayzBudget));
-    var div = document.createElement('div');
-    div.className = "day";
-    div.innerHTML = "День № " + i + " бюджет - " + dayzBudget;
-    document.body.appendChild(div);
-  }
+let budget = budgetInput.value;
+let dayz = dayInput.value;
+dayz = +dayz;
+if (isNaN(budget)) {
+alert('В поле бюджета введено не число!')
+}
+if (isNaN(dayz)) {
+alert('В поле дней введено не число!')
+}
+dayzBudget = Math.floor(budget / dayz);
+for (i = 1; i < dayz + 1; i++) {
+
+let div = document.createElement('div');
+div.className = "day";
+div.innerHTML = "День № " + i + " бюджет - " + dayzBudget;
+document.body.appendChild(div);
+}
 
 });
